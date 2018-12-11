edd2976bc958cd484c482b560b3257b84137a508
sth <765215342@qq.com>
2018-12-11 14:52:17 +0800

    Add stucts DataAttrInfo and DataRelInfo in catalog.h.

---
+++ src/catalog.h
@@ -0,0 +1,70 @@
+#ifndef CATALOG_H
+#define CATALOG_H
+
+#include "naivedb.h"
+#include <cstring>
+
+struct DataRelInfo
+{
+    DataRelInfo()
+    {
+        memset(relName, 0, MAXNAME + 1);
+    }
+
+    DataRelInfo(char* buf) // construct DataRelInfo from buf
+    {
+        memcpy(this, buf, sizeof(DataRelInfo));
+    }
+
+    DataRelInfo(const DataRelInfo& tmp)
+    {
+        memcpy(this, &tmp, sizeof(DataRelInfo));
+    }
+
+    DataRelInfo& operator=(const DataRelInfo& tmp)
+    {
+        if (this != &tmp)
+            memcpy(this, &tmp, sizeof(DataRelInfo));
+        return *this;
+    }
+    
+    int recordSize;
+    int attrCount;
+    char relName[MAXNAME + 1];
+};
+
+struct DataAttrInfo
+{
+    DataAttrInfo()
+    {
+        offset = -1;
+        indexNo = -1;
+        memset(relName, 0, MAXNAME + 1);
+        memset(attrName, 0, MAXNAME + 1);
+    }
+
+    DataAttrInfo(char* buf) // construct DataRelInfo from buf
+    {
+        memcpy(this, buf, sizeof(DataAttrInfo));
+    }
+
+    DataAttrInfo(const DataAttrInfo& tmp)
+    {
+        memcpy(this, &tmp, sizeof(DataAttrInfo));
+    }
+
+    DataAttrInfo& operator=(const DataAttrInfo& tmp)
+    {
+        if (this != &tmp)
+            memcpy(this, &tmp, sizeof(DataAttrInfo));
+        return *this;
+    }
+    int offset;
+    AttrType attrType;
+    int attrLength;
+    int indexNo; // index number, -1 means no index
+    char relName[MAXNAME + 1];
+    char attrName[MAXNAME + 1];
+};
+
+#endif //CATALOG_H
